f8108149e64fbad7cab569940398a7f1671b1aa1
kidneyweak <kidneyweak@gmail.com>
2023-12-11 15:39:08 +0800

    feat: zk mental poker

--- packages/contract/src/mental.ts
+++ packages/contract/src/mental.ts
@@ -18,61 +18,54 @@ await isReady
 
 class Mental extends SmartContract {
   @state(Cipher) c1 = State<Cipher>()
-  @state(Cipher) c2 = State<Cipher>()
-  @state(Cipher) c3 = State<Cipher>()
-  // @state(Cipher)
-
+  @state(Cipher) ans = State<Cipher>()
+  @state(Field) pk = State<Field>()
   @state(Field) result = State<Field>()
 
-  @method encrypt (m1: Field, m2: Field, pk: Field) {
-    this.c1.set(ElGamalFF.encrypt(m1, pk))
-    this.c2.set(ElGamalFF.encrypt(m2, pk))
+  @method setPubKey (pk: Field) {
+    this.pk.set(pk)
+    this.c1.set(ElGamalFF.encrypt(Field(1), pk))
   }
 
   // calculate shuffle
-  @method multiplyCipher () {
+  @method shuffleValue (randomValue: Field) {
     const c1 = this.c1.get()
     this.c1.assertEquals(c1)
 
-    const c2 = this.c2.get()
-    this.c2.assertEquals(c2)
+    const pk = this.pk.get()
+    this.pk.assertEquals(pk)
 
-    const c3 = this.c3.get()
-    this.c3.assertEquals(c3)
-
-    const product = c1.mul(c2)
-    this.c3.set(product)
+    const product = c1.mul(ElGamalFF.encrypt(randomValue, pk))
+    this.ans.set(product)
   }
 
   @method decrypt (secretKey: Field) {
-    const result = this.c3.get()
-    this.c3.assertEquals(result)
+    const result = this.ans.get()
+    this.ans.assertEquals(result)
 
     const plainText = ElGamalFF.decrypt(result, secretKey)
     this.result.set(plainText as any)
   }
 }
 
+// test
 const Local = Mina.LocalBlockchain({ proofsEnabled: doProofs })
 Mina.setActiveInstance(Local)
 
 // a test account that pays all the fees, and puts additional funds into the zkapp
 const { privateKey: senderKey, publicKey: sender } = Local.testAccounts[0]
+const { privateKey: userKey, publicKey: user } = Local.testAccounts[1]
 
 // the zkapp account
 const zkappKey = PrivateKey.random()
 const zkappAddress = zkappKey.toPublicKey()
 
 const zkapp = new Mental(zkappAddress)
-// console.log(zkapp)
 const { pk, sk } = ElGamalFF.generateKeys()
 
-const m1 = Field(5)
-const m2 = Field(10)
-console.log(m1)
 await Mental.compile()
 
-console.log("deploy")
+console.log("Admin")
 let tx = await Mina.transaction(sender, () => {
   AccountUpdate.fundNewAccount(sender)
   zkapp.deploy({ zkappKey })
@@ -80,19 +73,20 @@ let tx = await Mina.transaction(sender, () => {
 await tx.prove()
 await tx.sign([senderKey]).send()
 
-console.log("set and encrypt")
+console.log("set Public Key")
 tx = await Mina.transaction(sender, () => {
-  zkapp.encrypt(m1, m2, pk)
+  zkapp.setPubKey(pk)
 })
 await tx.prove()
 await tx.sign([senderKey]).send()
 
-console.log("multiply")
-tx = await Mina.transaction(sender, () => {
-  zkapp.multiplyCipher()
+console.log("User Shuffle")
+tx = await Mina.transaction(user, () => {
+  const randomValue = Math.floor(Math.random() * 10) + 1
+  zkapp.shuffleValue(Field(randomValue))
 })
 await tx.prove()
-await tx.sign([senderKey]).send()
+await tx.sign([userKey]).send()
 
 console.log("decrypt")
 tx = await Mina.transaction(sender, () => {
@@ -101,6 +95,7 @@ tx = await Mina.transaction(sender, () => {
 await tx.prove()
 await tx.sign([senderKey]).send()
 
-zkapp.result.get().assertEquals(m1.add(m2))
+console.log(zkapp.result.get())
+// zkapp.result.get().assertEquals(2)
 
 shutdown()
