91409fd21ba2b317bdc8a612e85a764257188a78
kidneyweakx <kidneyweakx@gmail.com>
2024-01-15 01:23:59 +0800

    feat(contract): run a test

--- packages/contract/src/mental.test.ts
+++ packages/contract/src/mental.test.ts
@@ -7,29 +7,30 @@ import {
   shutdown,
 } from "o1js"
 import { ElGamalFF } from "o1js-elgamal"
-import { Mental } from "./mental.js"
+import { Mental } from "./mental"
 
 const doProofs = true
 
 await isReady
 
-// Test accounts
+// test
+const Local = Mina.LocalBlockchain({ proofsEnabled: doProofs })
+Mina.setActiveInstance(Local)
+
+// a test account that pays all the fees, and puts additional funds into the zkapp
 const { privateKey: senderKey, publicKey: sender } = Local.testAccounts[0]
 const { privateKey: userKey, publicKey: user } = Local.testAccounts[1]
 
-// zkApp account
+// the zkapp account
 const zkappKey = PrivateKey.random()
 const zkappAddress = zkappKey.toPublicKey()
 
-console.log("ZkApp Address:", zkappAddress)
-
-// Instantiate zkApp contract
+console.log(zkappAddress)
 const zkapp = new Mental(zkappAddress)
 const { pk, sk } = ElGamalFF.generateKeys()
 
 await Mental.compile()
 
-// Deploy zkApp and set public key
 console.log("Admin")
 let tx = await Mina.transaction(sender, () => {
   AccountUpdate.fundNewAccount(sender)
@@ -38,40 +39,45 @@ let tx = await Mina.transaction(sender, () => {
 await tx.prove()
 await tx.sign([senderKey]).send()
 
-console.log("Set Public Key")
+console.log("set Public Key")
 tx = await Mina.transaction(sender, () => {
   zkapp.setPubKey(pk)
 })
 await tx.prove()
 await tx.sign([senderKey]).send()
 
-// User shuffles
-console.log("User Shuffle")
-tx = await Mina.transaction(user, () => {
-  const randomValue = Math.floor(Math.random() * 10) + 1
-  zkapp.shuffleValue(Field(randomValue))
-})
-await tx.prove()
-await tx.sign([userKey]).send()
-
-// Decrypt
-console.log("Decrypt")
+console.log("Start Game")
+const gameId = Field(1)
 tx = await Mina.transaction(sender, () => {
-  zkapp.decrypt(sk)
+  zkapp.startGame(gameId)
 })
 await tx.prove()
 await tx.sign([senderKey]).send()
 
-console.log("Result after decryption:", zkapp.result.get())
+console.log("User Shuffle")
+const randomValue = Field(5)
+tx = await Mina.transaction(user, () => {
+  zkapp.shuffleValue(gameId, randomValue)
+})
+await tx.prove()
+await tx.sign([userKey]).send()
 
-// Reset
-console.log("Reset")
+console.log("decrypt")
+tx = await Mina.transaction(sender, () => {
+  zkapp.decrypt(sk, gameId)
+})
+await tx.prove()
+await tx.sign([senderKey]).send()
+
+console.log(zkapp.result.get())
+// zkapp.result.get().assertEquals(5)
+
+console.log("reset")
 tx = await Mina.transaction(sender, () => {
   zkapp.reset()
 })
 await tx.prove()
 await tx.sign([senderKey]).send()
 
-console.log("Result after reset:", zkapp.result.get())
-
+console.log(zkapp.result.get())
 shutdown()
