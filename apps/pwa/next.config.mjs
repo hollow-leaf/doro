b68965243c425fc2ce8ec4e3fe65186208e6dd71
kidneyweakx <kidneyweakx@gmail.com>
2024-02-26 23:21:43 +0800

    fix(pwa): cloudflare page export output

--- apps/pwa/next.config.mjs
+++ apps/pwa/next.config.mjs
@@ -11,6 +11,7 @@ const withSerwist = withSerwistInit({
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
 };
 
 export default withSerwist(nextConfig);
