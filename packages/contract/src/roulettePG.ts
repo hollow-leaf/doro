347c534a9ca9e433d2537c186eb52e56577b8167
kidneyweakx <kidneyweakx@gmail.com>
2024-03-14 22:45:56 +0800

    feat(contract): update zkPG smart contract

--- packages/contract/src/roulettePG.ts
+++ packages/contract/src/roulettePG.ts
@@ -1,10 +1,15 @@
 import {
   Field,
+  state,
+  State,
   ZkProgram,
   SelfProof,
   Provable,
   Bool,
+  SmartContract,
+  method,
 } from "o1js"
+import { Cipher, ElGamalFF } from "o1js-elgamal"
 
 export const RoulettePG = ZkProgram({
   name: "roulette-program",
@@ -33,5 +38,39 @@ export const RoulettePG = ZkProgram({
         )
       },
     },
+    shuffle: {
+      privateInputs: [SelfProof, Field],
+
+      method (messageNumber: Field, earlierProof: SelfProof<Field, Field>, message: Field) {
+        // messageNumber.set(msgNumber)
+        const isValid = Bool(true)
+        return Provable.if(
+          isValid,
+          Field,
+          messageNumber,
+          earlierProof.publicOutput,
+        )
+      },
+    },
   },
 })
+
+const { verificationKey } = await RoulettePG.compile()
+class RouletteProof extends ZkProgram.Proof(RoulettePG) {}
+export class RoulettePGContract extends SmartContract {
+  @state(Cipher) c1 = State<Cipher>()
+  @state(Field) pk = State<Field>()
+  @state(Field) result = State<Field>()
+
+  @method setPubKey (pk: Field) {
+    this.pk.set(pk)
+    this.c1.set(ElGamalFF.encrypt(Field(1), pk))
+  }
+
+  @method setResults (proof: RouletteProof) {
+    proof.verify()
+
+    const result = proof.publicOutput
+    this.result.set(result)
+  }
+}
